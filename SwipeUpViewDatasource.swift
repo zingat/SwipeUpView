3f5fc44dcfa14b3c326a1fbbc7be8e5f8f2161d3
cinaryusuf <cinaryusuf@zmac0011.local>
2018-07-02 15:43:01 +0300

    fixing sizing bug

--- SwipeUpViewDatasource.swift
+++ SwipeUpViewDatasource.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 
-public   protocol SwipeUpViewDatasource : class {
+public protocol SwipeUpViewDatasource : class {
     
     func firstOpenHeightPercentageIndex (_ swipeUpView : SwipeUpView) -> Int
     func heightPercentages (_ swipeUpView : SwipeUpView) -> [CGFloat]
     
     func hideHeaderButton (_ swipeUpView : SwipeUpView) -> Bool
     func heightOfHeaderButton (_ swipeUpView : SwipeUpView) -> CGFloat
+    func widthOfHeaderButton (_ swipeUpView : SwipeUpView) -> CGFloat
+    func colorOfHeaderButton (_ swipeUpView : SwipeUpView) -> UIColor
     func marginOfHeaderButton (_ swipeUpView : SwipeUpView) -> CGFloat
     
 }
